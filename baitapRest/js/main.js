7fcd909d9399330ad98121adcbc2501d87d94d17
LyTrungHai <trunghai1610@gmail.com>
2021-11-29 22:00:49 +0700

    update

--- baitapRest/js/main.js
+++ baitapRest/js/main.js
@@ -1,24 +1,26 @@
-//Lấy giá trị từ form
-let inputKhoi1 = () => {
-  var toan = parseFloat(document.getElementById("inputToan").value);
-  var ly = parseFloat(document.getElementById("inputLy").value);
-  var hoa = parseFloat(document.getElementById("inputHoa").value);
-  let result = averageFunc(toan, ly, hoa);
-  document.getElementById("hienThi1").innerHTML = "Điểm trung bình: " + result;
-};
-document.getElementById("btnTinh1").onclick = inputKhoi1;
+// khối 1
 
-let inputKhoi2 = () => {
-  var van = parseFloat(document.getElementById("inputVan").value);
-  var su = parseFloat(document.getElementById("inputSu").value);
-  var dia = parseFloat(document.getElementById("inputDia").value);
-  var eng = parseFloat(document.getElementById("inputEng").value);
-  let result = averageFunc(van, su, dia, eng);
-  document.getElementById("hienThi2").innerHTML = "Điểm trung bình: " + result;
-  // console.log('result', result);
+let average1 = () => {
+  var toan = parseFloat(document.getElementById("inpToan").value);
+  var ly = parseFloat(document.getElementById("inpLy").value);
+  var hoa = parseFloat(document.getElementById("inpHoa").value);
+  let result = averageFunc(toan, ly, hoa);
+  document.getElementById("tbKhoi1").innerHTML = "Điểm trung bình: " + result;
 };
-document.getElementById("btnTinh2").onclick = inputKhoi2;
-//Tính trung bình
+document.getElementById("btnKhoi1").onclick = average1;
+
+// khoi 2
+let average2 = () => {
+  var van = parseFloat(document.getElementById("inpVan").value);
+  var su = parseFloat(document.getElementById("inpSu").value);
+  var dia = parseFloat(document.getElementById("inpDia").value);
+  var eng = parseFloat(document.getElementById("inpEnglish").value);
+  let result = averageFunc(van, su, dia, eng);
+  document.getElementById("tbKhoi2").innerHTML = "Điểm trung bình: " + result;
+};
+document.getElementById("btnKhoi2").onclick = average2;
+//
+
 let averageFunc = (...inputs) => {
   let sum = 0,
     average;
